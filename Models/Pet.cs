267886598564894305481fb71198a1b348b2f700
JorgeAlejo <jmejiaholguin@gmail.com>
2024-11-28 22:12:39 -0500

    DateUpdate

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -1,6 +1,8 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json;
 using System.Text.Json.Serialization;
+
 
 public class Pet{
     public Guid Id { get; set; } = Guid.NewGuid();
@@ -15,6 +17,7 @@ public class Pet{
     public string Gender { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Se requiere una fecha de nacimiento.")]
+    [JsonConverter(typeof(JsonDateConverter))]
     [DataType(DataType.Date)]
     [CustomValidation(typeof(Pet), nameof(ValidateBirthDate))]
     [Column(TypeName = "date")]
@@ -36,4 +39,23 @@ public class Pet{
         if (birthDate > DateTime.Today) return new ValidationResult("La fecha de nacimiento no puede ser en el futuro.");
         return ValidationResult.Success;
     }
+
+    public class JsonDateConverter : JsonConverter<DateTime>
+{
+    private const string DateFormat = "dd/MM/yyyy";
+
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (DateTime.TryParseExact(reader.GetString(), DateFormat, null, System.Globalization.DateTimeStyles.None, out var date))
+        {
+            return date;
+        }
+        throw new JsonException("Invalid date format.");
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString(DateFormat));
+    }
+}
 }
